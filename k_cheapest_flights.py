db12422278421eb2504b4d9318256bb5560523ae
rbhargav01 <42813856+rbhargav01@users.noreply.github.com>
2020-07-11 12:04:33 +0530

    Update k_cheapest_flights.py

--- k_cheapest_flights.py
+++ k_cheapest_flights.py
@@ -3,23 +3,26 @@ import heapq
 #mumbai_to_delhi: list of integers
 #k: integer
 def k_cheap_flights(delhi_to_mumbai,mumbai_to_delhi,k):
-    delhi_to_mumbai.sort()
+    
+    #If the given arrays are already sorted then the sorting functions are not necessary and the time complexity becomes = klog(k)
+    
+    delhi_to_mumbai.sort()                                                              #Sorting the given list of fares
     mumbai_to_delhi.sort()
     heap=[]
-    heap.append([delhi_to_mumbai[0]+mumbai_to_delhi[0],0,0])
+    heap.append([delhi_to_mumbai[0]+mumbai_to_delhi[0],0,0])                            #Appending the cheapest fares to the heap with the indexes of 1st array and the indexes of the second array
     ans=[]
     i1=0
     while(len(heap)>0 and i1<k):
-        cur=heapq.heappop(heap)
-        i=cur[1]
-        j=cur[2]
+        cur=heapq.heappop(heap)                                                         #Retrieving the cheapest sum pair
+        i=cur[1]                                                                        #Retrieving the index of Delhi to Mumbai flight as i
+        j=cur[2]                                                                        #Retrieving the index of Mumbai to Delhi flight as j
         if(i<len(delhi_to_mumbai)-1):
-            heapq.heappush(heap,[delhi_to_mumbai[i+1]+mumbai_to_delhi[j],i+1,j])
+            heapq.heappush(heap,[delhi_to_mumbai[i+1]+mumbai_to_delhi[j],i+1,j])        #Adding the (i+1,j)th element to the heap
         if(j<len(mumbai_to_delhi)-1):
-            heapq.heappush(heap,[delhi_to_mumbai[i]+mumbai_to_delhi[j+1],i,j+1])
-        if(len(ans)>0 and [delhi_to_mumbai[i],mumbai_to_delhi[j]]==ans[-1]):
+            heapq.heappush(heap,[delhi_to_mumbai[i]+mumbai_to_delhi[j+1],i,j+1])        #Adding the (i,j+1)th element to the heap
+        if(len(ans)>0 and [delhi_to_mumbai[i],mumbai_to_delhi[j]]==ans[-1]):            #To avoid repeatition of the same elements
             continue
-        ans.append([delhi_to_mumbai[i],mumbai_to_delhi[j]])
+        ans.append([delhi_to_mumbai[i],mumbai_to_delhi[j]])                             #Adding the pair to the ans array
         i1+=1
     return ans
 
