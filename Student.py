f0e3f8b1aa090a2e771ea3a4e67ac2b714473e3b
rbhargav01 <42813856+rbhargav01@users.noreply.github.com>
2020-07-11 11:52:41 +0530

    Update Student.py
    Question:
    Consider the following class with the following attributes:
    Class Student {
    String name;
    int age;
    int marks;
    String rollNumber;
    } 
    
    You are given a large collection of such Student objects and you need to sort it by a user-specified criteria. The criteria can be any permutation of any number of attributes. For example, you can be asked to sort this collection by:
    name
    name, age
    age, name
    marks, rollNumber, age
    age, marks, name, rollNumber
    and so on.
    
    Implement a method/function with the following signature:
    
    Collection<Student> sort(Collection<Student> students, String[] criteria)
    
    You can use any standard language methods such as Collection.sort, Array.sort, etc or other auxiliary methods of your own.
    Clearly state the time and space complexity of your solution.

--- Student.py
+++ Student.py
@@ -10,36 +10,36 @@ class Student:
 #sort_criteria can be given in any order but the input is case sensitive
 def sort_function(input_array,sort_criteria):
     temp=[]
-    for i in range(len(input_array)):
+    for i in range(len(input_array)):   #Creating an empty array for each student
         temp.append([])
     for i in range(len(sort_criteria)+1):
         if(i==len(sort_criteria)):
-            for j in range(len(input_array)):
+            for j in range(len(input_array)):   #Append the index of the each student at the end of jth row
                 temp[j].append(j)
             break
-        if(sort_criteria[i]=="name"):
+        if(sort_criteria[i]=="name"):   #Add name of each student to the jth row as the ith element to be sorted
             for j in range(len(input_array)):
                 temp[j].append(input_array[j].name)
-        if(sort_criteria[i]=="age"):
+        if(sort_criteria[i]=="age"):    #Add age of each student to the jth row as the ith element to be sorted
             for j in range(len(input_array)):
                 temp[j].append(input_array[j].age)
-        if(sort_criteria[i]=="marks"):
+        if(sort_criteria[i]=="marks"):  #Add marks of each student to the jth row as the ith element to be sorted
             for j in range(len(input_array)):
                 temp[j].append(input_array[j].marks)
-        if(sort_criteria[i]=="rollNumber"):
+        if(sort_criteria[i]=="rollNumber"): #Add rollNumber of each student to the jth row as the ith element to be sorted
             for j in range(len(input_array)):
                 temp[j].append(input_array[j].rollNumber)
-    temp.sort()
-    for i in range(len(temp)):
+    temp.sort() #Sorting the created array according to the given criteria
+    for i in range(len(temp)): #Adding the whole object as the answer instead of the specified criteria
         temp[i]=input_array[temp[i][-1]]
     return temp
 
 
 input_array=[]
-n=int(input())
+n=int(input("Enter the number of students: "))
 for i in range(n):
-    inp=input().split()
+    inp=input("Enter details of student"+str(i+1)+" : ".split()
     s=Student(inp[0],inp[1],inp[2],inp[3])
     input_array.append(s)
-sort_criteria=input().split()
+sort_criteria=input("Enter sort criteria : ").split()
 print(sort_function(input_array,sort_criteria))
